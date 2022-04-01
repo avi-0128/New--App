72800ee8d33f2357df5d1f77d8543054bd569814
avi-0128 <t-avinash.mamidi@ocltp.com>
2022-04-01 13:07:34 +0530

    commit

--- react-app/src/components/Home.js
+++ react-app/src/components/Home.js
@@ -1,14 +1,12 @@
 import React, { useState } from "react";
 import { getCurrentUser } from "./CurrentUser";
-import DuplicateMovies from "./DuplicateMovies";
 import MoviesHome from "./MoviesHome";
-import RegDuplicate from "./RegDuplicate";
 import Register from "./Register";
 
 const Home = () => {
   const currentUser = getCurrentUser()
  console.log(currentUser)
-  return <>{ currentUser ? <MoviesHome /> : <RegDuplicate />}</>;
+  return <>{ currentUser ? <MoviesHome /> : <Register />}</>;
 };
 
 export default Home;
